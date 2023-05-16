4024d8b8efc5392dcfc0082014171cb031ec1f41
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-16 13:51:23 -0700

    updated webhook route name

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::process::Command;
 use actix_web::{web, App, HttpServer};
+use std::process::Command;
 
 async fn handle_push_event(payload: String) -> String {
-    println!("Received push event");
-    let output = Command::new("git").arg("pull").output().expect("Error pulling");
+    println!("Received new event: {}", payload);
+    let output = Command::new("git")
+        .arg("pull")
+        .output()
+        .expect("Error running git pull");
 
-    println!("output: {}", String::from_utf8_lossy(&output.stdout));
+    println!("command output: {}", String::from_utf8_lossy(&output.stdout));
 
     "Push event received".to_string()
 }
@@ -13,11 +16,8 @@ async fn handle_push_event(payload: String) -> String {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8080";
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::post().to(handle_push_event))
-    })
-    .bind(addr)?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/webhook", web::post().to(handle_push_event)))
+        .bind(addr)?
+        .run()
+        .await
 }
